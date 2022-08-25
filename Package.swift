2d2aeb16eefa7db711a7fa103b9fc15406c45f4c
Abdullah Cheema <abdullahcheema.macmads@gmail.com>
2022-08-25 20:25:28 +0500

    publishing with all dependencies

--- Package.swift
+++ Package.swift
@@ -14,11 +14,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios", from: "3.4.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.2"),
+        .package(url: "https://github.com/truid-ai/T5AirSnapPackage", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on
         .binaryTarget(name: "TruID", path: "./Sources/TruID.xcframework")
-
     ]
 )
