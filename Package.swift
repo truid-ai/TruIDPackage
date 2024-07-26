d06bb0d648d556fb8f316d32c15fd8dbc902475f
subhant5 <subhantariqbutt47@gmail.com>
2024-07-26 16:07:08 +0500

    Remove dependencies

--- Package.swift
+++ Package.swift
@@ -16,10 +16,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/airbnb/lottie-ios", from: "3.4.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.2"),
-        .package(url: "https://github.com/truid-ai/T5AirSnapPackage", branch: "main")
         .package(url: "https://github.com/kewlbear/TensorFlowLiteSwift.git", .branch("master"))
-        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.0")
-],
+        ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on
