482ee8910f32190610e731fae00564522a986ed1
subhant5 <subhantariqbutt47@gmail.com>
2024-07-26 17:56:30 +0500

    remove dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/airbnb/lottie-ios", from: "3.4.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.2"),
-        .package(url: "https://github.com/truid-ai/T5AirSnapPackage", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
