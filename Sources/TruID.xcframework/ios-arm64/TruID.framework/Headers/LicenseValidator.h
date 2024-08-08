746cb7a75cea2efe86150703c27636ca1f31cd1c
Hamza <muhammadhamzam1486@gmail.com>
2024-08-08 18:06:46 +0500

    again push

---
+++ Sources/TruID.xcframework/ios-arm64/TruID.framework/Headers/LicenseValidator.h
@@ -0,0 +1,18 @@
+//
+//  LicenseValidator.h
+//  TruID
+//
+//  Created by truID on 25/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LicenseValidator : NSObject
+
++ (BOOL)validateLicense;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sources/TruID.xcframework/ios-arm64/TruID.framework/Headers/OpenCVWrapper.h
@@ -0,0 +1,26 @@
+//
+//  OpenCVWrapper.h
+//  TruID
+//
+//  Created by truID on 14/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OpenCVWrapper : NSObject
++ (NSString *)getOpenCVVersion;
++ (UIImage *)grayscaleImg:(UIImage *)image;
++ (UIImage *)convertToRGBFromGrayscale:(UIImage *)image;
++ (UIImage *)resizeImg:(UIImage *)image
+                      :(int)width
+                      :(int)height
+                      :(int)interpolation;
++ (UIImage *)binarize:(UIImage *)image;
++ (UIImage *)applyPill:(UIImage *)image :(UIImage *)pillImage;
+
+@end
+
+NS_ASSUME_NONNULL_END
