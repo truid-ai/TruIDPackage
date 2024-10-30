16e1e6966eae4bcc2cc9ee6d48e4dbb8a4451ba9
Muhammad Hamza <57759769+CodeWhizHamza@users.noreply.github.com>
2024-10-30 20:45:28 +0500

    Remove all dependencies

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/airbnb/lottie-ios", from: "3.4.0"),
+        //.package(url: "https://github.com/airbnb/lottie-ios", from: "3.4.0"),
         //.package(url: "https://github.com/Alamofire/Alamofire", from: "5.6.2"),
     ],
     targets: [
